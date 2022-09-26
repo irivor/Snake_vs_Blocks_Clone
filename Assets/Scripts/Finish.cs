31c6c81a9128904c817ae61b056cd84c78bb7447
irivor <68502024+irivor@users.noreply.github.com>
2022-09-26 15:33:39 +0700

    add_sound_code

--- Assets/Scripts/Finish.cs
+++ Assets/Scripts/Finish.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class Finish : MonoBehaviour
 {
     public GameObject Win;
+    public AudioSource FinishSound;
+
+    void Start()
+    {
+        FinishSound = GetComponent<AudioSource>();
+    }
 
     public void OnCollisionEnter(Collision collision)
     {
+        FinishSound.Play();
         Win.SetActive(true);
     }
 }
