e4093fd7fbeccefc8616810704704a7e66b7ef0a
irivor <68502024+irivor@users.noreply.github.com>
2022-09-26 16:10:06 +0700

    Update NavigateMeu

--- Assets/Scripts/NavigateMenu.cs
+++ Assets/Scripts/NavigateMenu.cs
@@ -3,9 +3,15 @@ using UnityEngine.SceneManagement;
 
 public class NavigateMenu : MonoBehaviour
 {
-   
+    public void SwitchLevel(string SceneName)
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+
     public void ReloadLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
+
+
